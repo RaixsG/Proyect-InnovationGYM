b10db947b808165b8cf3f7f283474caf2bbb9ed5
RaixsG <jhardelloficial@gmail.com>
2023-09-10 14:12:29 -0500

    feat: Start Proyect

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<h1>Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<style>
+    :root {
+        --body-color: #222;
+        --body-secundari: #E4E9F7;
+        --text-color: #fafafa;
+        --tran-03: all 0.2s ease;
+        --tran-03: all 0.3s ease;
+        --tran-04: all 0.3s ease;
+        --tran-05: all 0.3s ease;
+    }
+    :global(*) {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+            Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+            sans-serif;
+    }
+
+    :global(body) {
+        min-height: 100vh;
+        background-color: #222;
+    }
+</style>
